5d31fe51d4b0c6900819cf6808b2934a97c660ab
kmthehippie <your_github_noreply_email@users.noreply.github.com>
2025-04-25 23:45:35 +0800

    Update app.js, setup route and folders

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+const indexRouter = require("./routes/indexRouter").router;
+app.use("/", indexRouter);
+
 app.listen(process.env.PORT, () => {
   console.log(`App is now listening on port ${process.env.PORT}`);
 });
