88a3dfe73ea8e9362a6dfa46d8b41fb067c418ff
kmthehippie <your_github_noreply_email@users.noreply.github.com>
2025-08-19 12:17:54 +0800

    Update cors Credentials config

--- config/corsCredentials.js
+++ config/corsCredentials.js
@@ -1,10 +1,48 @@
+// const allowedOrigins = require("../config/allowedOrigins");
+
+// const corsCredentials = (req, res, next) => {
+//   const origin = req.headers.origin;
+//   console.log("The allowed origins: ", allowedOrigins);
+
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, PATCH, DELETE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//   }
+
+//   if (req.method === "OPTIONS") {
+//     res.setHeader("Access-Control-Allow-Origin", origin); // Set CORS headers for preflight response
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, PUT, PATCH, DELETE"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Content-Type, Authorization"
+//     );
+//     res.setHeader("Access-Control-Allow-Credentials", true);
+//     res.sendStatus(200); // Preflight request successful
+//   } else {
+//     next();
+//   }
+// };
+
+// module.exports = corsCredentials;
+
 const allowedOrigins = require("../config/allowedOrigins");
 
 const corsCredentials = (req, res, next) => {
   const origin = req.headers.origin;
-  console.log("The allowed origins: ", allowedOrigins);
+  const isAllowed = allowedOrigins.includes(origin) || !origin;
 
-  if (allowedOrigins.includes(origin)) {
+  if (isAllowed) {
     res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader(
       "Access-Control-Allow-Methods",
@@ -17,21 +55,13 @@ const corsCredentials = (req, res, next) => {
     res.setHeader("Access-Control-Allow-Credentials", true);
   }
 
+  // Handle preflight requests by sending a 200 OK after headers are set
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", origin); // Set CORS headers for preflight response
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.sendStatus(200); // Preflight request successful
-  } else {
-    next();
+    return res.sendStatus(200);
   }
+
+  // For all other requests, pass control to the next middleware
+  next();
 };
 
 module.exports = corsCredentials;
