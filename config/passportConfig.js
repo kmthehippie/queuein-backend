90d309063bef770ac997bcf6b62b0c626dc21413
kmthehippie <your_github_noreply_email@users.noreply.github.com>
2025-11-05 15:40:52 +0800

    Add encrypt /decrypt for passport Config

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -3,6 +3,7 @@ const GoogleStrategy = require("passport-google-oauth20");
 const LocalStrategy = require("passport-local").Strategy;
 require("dotenv").config();
 const validatePw = require("./passwordUtils").validatePw;
+const { encrypt, decrypt } = require("../utils/encryption");
 
 const {
   getAccountEmail,
@@ -24,7 +25,8 @@ passport.use(
     async (req, email, password, done) => {
       try {
         console.log("Entering passport local strategy: ", req.body);
-        const account = await getAccountEmail(email);
+        const decryptEmail = decrypt(email);
+        const account = await getAccountEmail(decryptEmail);
         if (!account) {
           console.log("Authentication failed -- No such email found: ", email);
           return done(null, false, { error: "Invalid Email or Password" });
